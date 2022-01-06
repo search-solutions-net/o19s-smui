1ee23be0da8992d8b2db8a7c69528541e5b12395
paulb <paul@search-solutions.net>
2022-01-06 08:58:54 +0100

    Fix base path for port 4200

--- frontend/src/proxy.conf.js
+++ frontend/src/proxy.conf.js
@@ -3,9 +3,15 @@ const PROXY_CONFIG = {
     "target": "http://localhost:9000",
     "secure": false,
     "bypass": function (req) {
-      if (req && req.headers && req.headers.accept && req.headers.accept.indexOf("html") !== -1) {
+      if (req
+        && req.headers
+        && req.headers.accept
+        && req.headers.accept.indexOf("html") !== -1
+        && req.path
+        && req.path !== '/'
+      ) {
         console.log("Skipping proxy for browser request.");
-        return "/index.html";
+        return "/";
       }
     }
   }
